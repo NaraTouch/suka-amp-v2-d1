1c0ec7a736c5bce58e38ca1beecbb4a0c3076203
DESKTOP-361N8F5\USer <virak.sukadigital@gmail.com>
2022-03-22 16:25:38 +0700

    - Add input and style
    - Add button and style

--- components/DesktopForm.vue
+++ components/DesktopForm.vue
@@ -3,14 +3,22 @@
     <form class="d-login-form" method="post" action-xhr="/documentation/examples/api/submit-form-input-text-xhr" target="_top">
       <div>
         <div class="left-inner-addon">
-            <span class="form-control-feedback bg-gradient-1">
-              <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+            <span class="form-control-feedback bg-gradient-1 span-style">
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
                 <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
               </svg>
             </span>
-            <input type="password"
-                   class="form-control" 
-                   placeholder="Left icon" />
+            <input type="password" class="form-control input-custom-style" placeholder="input text" />
+
+             <span class="form-control-feedback bg-gradient-1 span-style">
+              <svg xmlns="http://www.w3.org/2000/svg" width="14" height="14" fill="currentColor" class="bi bi-person-fill" viewBox="0 0 16 16">
+                <path d="M3 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H3zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+              </svg>
+            </span>
+            <input type="password" class="form-control input-custom-style" placeholder="input text" />
+
+            <button class="btn btn-color">Masuk</button>
+            <button class="btn ">Masuk</button>
         </div>
     </div>
     </form>
@@ -28,21 +36,39 @@ export default {
   background-color: transparent;
   border: none;
   position: absolute;
-  line-height: 45px;
   text-align: center; 
   display: block;
   font-size: 14px;
   right: 60px; 
-  top: 0px;
+  top: 13px;
   bottom: 0px;
   .left-inner-addon {
     position: relative;
-    padding-bottom: 1em;
+    .span-style {
+       position: absolute;
+      padding: 0px 3px ;
+      margin: 0px -25px 0px 0px;
+      border-radius: 10px;
+      background-color: $rgb-light-1;
+    }
+    .input-custom-style {
+      @extend .input-gradient;
+      padding: 2px 13px 1px 22px;
+      border: 1px solid $rgb-light-1;
+      border-radius: 14px;
+    }
+    .btn {
+      border-radius: 10px;
+      border: 1px solid $rgb-light-1;
+    }
+    .btn-color {
+      background: $btn-color-1;
+      color: $color-white;
+    }
+    svg{ 
+      fill:  $color-white; 
+    }
   }
-  .left-inner-addon span {
-    position: absolute;
-    padding: 12px 12px;
-    pointer-events: none;
-  }
+  
 }
 </style>
