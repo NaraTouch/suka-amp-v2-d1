ebe6d8497941b3fcfb1ea0c20802f3bc9e15cf39
NaraTouch <sophonaratouch@gmail.com>
2022-03-22 11:35:00 +0700

    delete dist2

--- dist/_nuxt/static/1647922844/amp/about/payload.js
+++
@@ -1,1 +0,0 @@
-__NUXT_JSONP__("/amp/about", {data:[{}],fetch:{},mutations:void 0});
--- dist/_nuxt/static/1647922844/amp/state.js
+++
@@ -1,1 +0,0 @@
-window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1647922844",layout:"default",error:a,serverRendered:true,routePath:"\u002Famp",config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},http:{browserBaseURL:b}}}}(null,"\u002F"));
